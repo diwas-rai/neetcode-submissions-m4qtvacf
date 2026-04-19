9dc979c25ca490fee7e71a2afb3e61c07155eb45
Diwas Rai <diwasrai38@yahoo.com>
2026-04-20 00:23:13 +0100

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.py
@@ -0,0 +1,19 @@
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
+        self.min_stk = []
+
+    def push(self, val: int) -> None:
+        self.stk.append(val)
+        self.min_stk.append(min(val, val if not self.min_stk else self.min_stk[-1]))
+
+    def pop(self) -> None:
+        self.stk.pop()
+        self.min_stk.pop()
+
+    def top(self) -> int:
+        return self.stk[-1]
+
+    def getMin(self) -> int:
+        return self.min_stk[-1]
